e30cb4d13ae953df7db48d7d8c37c2ae1e66e673
tangtaoit <tangtaoit@githubim.com>
2023-08-09 13:56:59 +0800

    feat: add file api docs

--- modules/channel/1module.go
+++ modules/channel/1module.go
@@ -10,13 +10,18 @@ import (
 //go:embed sql
 var sqlFS embed.FS
 
+//go:embed swagger/api.yaml
+var swaggerContent string
+
 func init() {
 	register.AddModule(func(ctx interface{}) register.Module {
 		return register.Module{
+			Name: "channel",
 			SetupAPI: func() register.APIRouter {
 				return New(ctx.(*config.Context))
 			},
-			SQLDir: register.NewSQLFS(sqlFS),
+			SQLDir:  register.NewSQLFS(sqlFS),
+			Swagger: swaggerContent,
 		}
 	})
 }
--- modules/file/1module.go
+++ modules/file/1module.go
@@ -1,17 +1,24 @@
 package file
 
 import (
+	_ "embed"
+
 	"github.com/TangSengDaoDao/TangSengDaoDaoServerLib/config"
 	"github.com/TangSengDaoDao/TangSengDaoDaoServerLib/pkg/register"
 )
+
+//go:embed swagger/api.yaml
+var swaggerContent string
 
 func init() {
 
 	register.AddModule(func(ctx interface{}) register.Module {
 		return register.Module{
+			Name: "file",
 			SetupAPI: func() register.APIRouter {
 				return New(ctx.(*config.Context))
 			},
+			Swagger: swaggerContent,
 		}
 	})
 }
