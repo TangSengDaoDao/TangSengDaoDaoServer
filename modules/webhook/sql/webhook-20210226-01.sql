cd9d7407951b86b37594e8d35267899ecce320d7
tangtaoit <tangtaoit@githubim.com>
2023-09-21 22:07:48 +0800

    feat: add expire

--- modules/webhook/sql/webhook-20210226-01.sql
+++ modules/webhook/sql/webhook-20210226-01.sql
@@ -119,19 +119,3 @@ create table `message4`
 );
 
 CREATE UNIQUE INDEX message_id on `message4` (message_id);
-
-
-ALTER TABLE `message` ADD COLUMN expire integer not null DEFAULT 0 COMMENT '消息过期时长';
-ALTER TABLE `message` ADD COLUMN expire_at BIGINT not null DEFAULT 0 COMMENT '消息过期时间';
-
-ALTER TABLE `message1` ADD COLUMN expire integer not null DEFAULT 0 COMMENT '消息过期时长';
-ALTER TABLE `message1` ADD COLUMN expire_at BIGINT not null DEFAULT 0 COMMENT '消息过期时间';
-
-ALTER TABLE `message2` ADD COLUMN expire integer not null DEFAULT 0 COMMENT '消息过期时长';
-ALTER TABLE `message2` ADD COLUMN expire_at BIGINT not null DEFAULT 0 COMMENT '消息过期时间';
-
-ALTER TABLE `message3` ADD COLUMN expire integer not null DEFAULT 0 COMMENT '消息过期时长';
-ALTER TABLE `message3` ADD COLUMN expire_at BIGINT not null DEFAULT 0 COMMENT '消息过期时间';
-
-ALTER TABLE `message4` ADD COLUMN expire integer not null DEFAULT 0 COMMENT '消息过期时长';
-ALTER TABLE `message4` ADD COLUMN expire_at BIGINT not null DEFAULT 0 COMMENT '消息过期时间';
